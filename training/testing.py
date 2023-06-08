7f9b48dd18ed35498f6db2275d19561603b77e1c
Andrea Basile <andreabasile97@gmail.com>
2023-06-08 15:34:33 +0200

    Explaining percentile, modalities and edges

--- training/testing.py
+++ training/testing.py
@@ -2,41 +2,30 @@ from training.utilities import generate_tumor_segmentation_from_graph, predict, 
 import pickle 
 import torch
 import nibabel as nib
-from training.explainer import explain_graph
+from training.explainer import explain_nodes_by_class, explain_multiple_graphs
 import networkx as nx
 import json
 import dgl
 
-with open('full_dataset_with_id_006.pickle', 'rb') as f:
+with open('pickle_dataset/full_dataset_with_id_01.pickle', 'rb') as f:
     dataset = pickle.load(f)
 
 
-grafo_esempio = dataset[1][0]
-features = dataset[1][1]
-id = dataset[1][3]
+grafo_esempio = dataset[0][0]
+features = dataset[0][1]
+labels = dataset[0][2]
+id = dataset[0][3]
 
 print(id)
 
 from models.GATSage import GraphSage
 
-test_model = GraphSage(in_feats = 20, layer_sizes = [256, 256, 256, 256, 256, 256], n_classes = 4, aggregator_type='pool', dropout=0)
+test_model = GraphSage(in_feats = 20, layer_sizes = [256, 256, 256, 256, 256, 256], n_classes = 4, aggregator_type='pool', dropout=0.2)
 
-test_model.load_state_dict(torch.load('model_epoch_25.pth'))
-
-# predicted_labels = predict(grafo_esempio, features, test_model)
-
-# slic = nib.load(f'datasets/DGL_graphs/train_006/BraTS2021_{id}/BraTS2021_{id}_supervoxels.nii.gz').get_fdata()
-# labels = nib.load(f'datasets/DGL_graphs/train_006/BraTS2021_{id}/BraTS2021_{id}_label.nii.gz').get_fdata()
+test_model.load_state_dict(torch.load('model_epoch_25_graphSage.pth'))
 
 
-# tumor = generate_tumor_segmentation_from_graph(predicted_labels, slic)
+# class_node_masks = explain_nodes_by_class(test_model, grafo_esempio, features, labels)
+# print(class_node_masks)
 
-# _3Dplotter(tumor)
-# _3Dplotter(labels)
-
-
-
-# Explainer
-
-features_mask, edge_mask = explain_graph(test_model, grafo_esempio, features)
-print(max(edge_mask))
+class_node_masks_all_graphs = explain_multiple_graphs(test_model, dataset, num_graphs=1, nodes_per_class=50, random_seed=23)
